b7940c439db69ac0bb02843cb618ed61279afe93
João Victor <joao.victor@internit.net.br>
2025-12-02 23:00:50 -0300

    more adjust on front

--- ice-cream-front/components/LoginPageContent/index.tsx
+++ ice-cream-front/components/LoginPageContent/index.tsx
@@ -21,7 +21,7 @@ export function LoginPageContent({ dark }:LoginPageContentProps) {
     return (
         <>
             <div className={`px-4 w-full min-h-screen flex flex-col gap-3 justify-center items-center transition-all duration-500 ${dark ? 'bg-[#202020] text-white' : 'bg-[#fbfbfb] text-black'}`}>
-                <Image className="logo" src="/icon-192x192.png" width={192} height={192} alt="logo da sorveteria" />
+                <Image className="logo" src="/logo-2.png" width={192} height={192} alt="logo da sorveteria" />
                 <div className="flex flex-col gap-3">
                     <h1 className="welcome text-xl text-center font-bold">Bem vindo</h1>
                     <LoginForm />
